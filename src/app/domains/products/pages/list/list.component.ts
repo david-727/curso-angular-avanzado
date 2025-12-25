10e33eb7ddc335d9506d155becc981ddae4f9956
David Huillca <david.vid727@gmail.com>
2025-12-25 14:17:11 -0500

    refactor: simplify arrow function syntax and clean up code formatting

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -42,7 +42,7 @@ export default class ListComponent implements OnInit, OnChanges {
 
   private getProducts() {
     this.productService.getProducts(this.category_id).subscribe({
-      next: (products) => {
+      next: products => {
         this.products.set(products);
       },
     });
@@ -50,7 +50,7 @@ export default class ListComponent implements OnInit, OnChanges {
 
   private getCategories() {
     this.categoryService.getAll().subscribe({
-      next: (data) => {
+      next: data => {
         this.categories.set(data);
       },
     });
