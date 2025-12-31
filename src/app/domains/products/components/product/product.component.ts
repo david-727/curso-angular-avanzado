2e3011a1de58422021e99eb712f75365242affc8
David Huillca <david.vid727@gmail.com>
2025-12-31 00:33:36 -0500

    feat: optimize image loading in product and product detail components using ngSrc and lazy loading

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,12 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { Product } from '@shared/models/product.model';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [CommonModule, TimeAgoPipe, RouterLinkWithHref],
+  imports: [CommonModule, TimeAgoPipe, RouterLinkWithHref, NgOptimizedImage],
   templateUrl: './product.component.html',
 })
 export class ProductComponent {
