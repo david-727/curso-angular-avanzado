651dc03e668c6164c4ca75214b730c40d0dcfb4d
David Huillca <david.vid727@gmail.com>
2026-03-02 21:58:03 -0500

    Interoperabilidad con toSignal

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { CounterComponent } from '@shared/components/counter/counter.component';
 import { WaveAudioComponent } from '@info/components/wave-audio/wave-audio.component';
 import { HighlightDirective } from '@shared/directives/highlight.directive';
 import { FormsModule } from '@angular/forms';
+import { BehaviorSubject, delay, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -20,6 +22,16 @@ export default class AboutComponent {
   duration = signal(1000);
   message = signal('Hola');
 
+  obsWithInit$ = new BehaviorSubject('David');
+  $withInit = toSignal(this.obsWithInit$, {
+    requireSync: true,
+  });
+
+  obsWithoutInit$ = new Subject<string>();
+  $withoutInit = toSignal(this.obsWithoutInit$.pipe(delay(3000)), {
+    initialValue: '----',
+  });
+
   changeDuration(event: Event) {
     const input = event.target as HTMLInputElement;
     this.duration.set(input.valueAsNumber);
@@ -27,5 +39,13 @@ export default class AboutComponent {
 
   changeMessage(event: string) {
     console.log('event', event);
+  }
+
+  emitWithInit() {
+    this.obsWithInit$.next('David Huillca');
+  }
+
+  emitWithoutInit() {
+    this.obsWithoutInit$.next('You are the best 🚀');
   }
 }
