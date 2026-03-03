b3e5fd944ec7be9d7c9370b079dc7fdfc0eaa82b
David Huillca <david.vid727@gmail.com>
2026-03-02 22:21:37 -0500

    Conversion de Observables a Signals con toSignal

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  inject,
-  signal,
-  OnInit,
-  OnChanges,
-  input
-} from '@angular/core';
+import { Component, inject, signal, OnChanges, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLinkWithHref } from '@angular/router';
 import { ProductComponent } from '@products/components/product/product.component';
@@ -13,24 +6,27 @@ import { Product } from '@shared/models/product.model';
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
-import { Category } from '@shared/models/category.model';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-list',
   imports: [CommonModule, ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
 })
-export default class ListComponent implements OnInit, OnChanges {
-  products = signal<Product[]>([]);
-  categories = signal<Category[]>([]);
+export default class ListComponent implements OnChanges {
   private cartService = inject(CartService);
   private productService = inject(ProductService);
   private categoryService = inject(CategoryService);
   readonly slug = input<string>();
 
-  ngOnInit() {
-    this.getCategories();
-  }
+  products = signal<Product[]>([]);
+  loadingProducts = signal(false);
+  errorProducts = signal('');
+
+  loadingCategories = signal(false);
+  $categories = toSignal(this.categoryService.getAll(), {
+    initialValue: [],
+  });
 
   ngOnChanges() {
     this.getProducts();
@@ -41,18 +37,21 @@ export default class ListComponent implements OnInit, OnChanges {
   }
 
   private getProducts() {
+    this.loadingProducts.set(true);
     this.productService.getProducts({ category_slug: this.slug() }).subscribe({
       next: products => {
         this.products.set(products);
+        this.loadingProducts.set(false);
+      },
+      error: error => {
+        this.errorProducts.set(error.message);
+        this.loadingProducts.set(false);
+        this.products.set([]);
       },
     });
   }
 
-  private getCategories() {
-    this.categoryService.getAll().subscribe({
-      next: data => {
-        this.categories.set(data);
-      },
-    });
+  resetCategories() {
+    this.$categories.reset([]);
   }
 }
